e9129a83a56c17478213736cb623bb23f8cc5727
Karnak19 <basile64.v@gmail.com>
2019-06-27 00:56:58 +0200

    wip

--- frontvue/src/components/Nav.vue
+++ frontvue/src/components/Nav.vue
@@ -1,13 +1,16 @@
 <template>
-  <b-navbar toggleable="lg" variant="info">
+  <b-navbar toggleable="lg" variant="transparent">
     <b-navbar-brand to="/">Wild Code League</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item to="/">Link</b-nav-item>
-        <b-nav-item to="about">Disabled</b-nav-item>
+        <b-nav-item to="/campus">Campus</b-nav-item>
+        <b-nav-item to="/history">Matches history</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/about">About</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -33,3 +36,9 @@ export default {
   }
 };
 </script>
+
+<style>
+a.router-link-exact-active {
+  color: rgb(21, 47, 197) !important;
+}
+</style>
---
+++ frontvue/src/views/Campuses.vue
@@ -0,0 +1,16 @@
+<template>
+  <app-layout>
+    <b-col>hello campus page</b-col>
+  </app-layout>
+</template>
+<script>
+import LayoutVue from "@/Layout.vue";
+export default {
+  name: "Campus page",
+  components: {
+    "app-layout": LayoutVue
+  }
+};
+</script>
+
+<style></style>
