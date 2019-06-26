496dac538f265b1a08d8ff8fd2754de37c12934a
Karnak19 <basile64.v@gmail.com>
2019-06-26 17:29:50 +0200

    added husky for code formatting on pre-commit

---
+++ frontvue/src/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-layout row wrap>
+        <app-nav />
+        <slot />
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Nav from "@/components/Nav";
+
+export default {
+  components: {
+    "app-nav": Nav
+  }
+};
+</script>
